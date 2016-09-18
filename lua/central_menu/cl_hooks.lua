a52584177e07e37af3e8fc20f0639d1649bc012b
Devul <devultj@gmail.com>
2016-09-18 15:31:47 +0100

    Added binds for f1 to f4 on clientside

--- lua/central_menu/cl_hooks.lua
+++ lua/central_menu/cl_hooks.lua
@@ -6,12 +6,26 @@
   Developer information: {{ user_id }} : {{ script_id }} : {{ script_version_id }}
 ]]--
 
+local FBinds = {
+    ["gm_showhelp"] = "ShowHelp",
+    ["gm_showteam"] = "ShowTeam",
+    ["gm_showspare1"] = "ShowSpare1",
+    ["gm_showspare2"] = "ShowSpare2"
+}
+
 local FKeyToHook = {
     ["F1"] = "ShowHelp",
     ["F2"] = "ShowTeam",
     ["F3"] = "ShowSpare",
     ["F4"] = "ShowSpare2"
 }
+
+hook.Add( "PlayerBindPress", "cm", function( client, bind, pressed )
+    local _bind = string.match( string.lower( bind ), "gm_[a-z]+[12]?" )
+    if _bind and FBinds[ _bind ] then
+        hook.Call( FBinds[ _bind ], GAMEMODE )
+    end
+end )
 
 hook.Add( FKeyToHook[ cm.getUnEditableData( "menu_key", "F1" ) ] or "", "cm", function()
     if not cm.frame or not cm.frame:IsVisible() or not cm.frame.background.reverseFade then
